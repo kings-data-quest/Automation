cca45a8ad6c317c8275701254e63726cba626d09
Kingsley Ebube Onoh <kingsley.ebube.onoh@gmail.com>
2023-04-05 03:32:58 +0100

    setting up git

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+
+def add(a:int, b: int):
+    """_summary_
+
+    Args:
+        a (int): _description_
+        b (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a + b
+
+c = add(3,5)
+print(c)
